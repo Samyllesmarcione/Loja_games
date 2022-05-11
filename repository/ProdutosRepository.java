3d8da8217d96f9092c25609c006cb9b938a072bb
Samyllesmarcione <101689015+Samyllesmarcione@users.noreply.github.com>
2022-05-10 23:59:35 -0300

    Add files via upload

---
+++ repository/ProdutosRepository.java
@@ -0,0 +1,21 @@
+package com.generation.lojadegames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.lojadegames.model.Produtos;
+
+    @Repository
+    public interface ProdutosRepository extends JpaRepository<Produtos, Long> {
+	public List <Produtos> findAllByNomeProdutoContainingIgnoreCase (@Param("nomeProduto")String nomeProduto);
+
+}
+
+
+
+
+
+
